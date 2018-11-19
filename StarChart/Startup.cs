86d8b78ef2b5dc1d6acd97b0fdedf29d38026e50
PKidd <paul.kidd@sportscotland.org.uk>
2018-11-19 22:10:15 +0000

    Testing Push

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -14,6 +14,7 @@ namespace StarChart
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
